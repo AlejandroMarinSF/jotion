2ee49dfcab6f4dd489e3a9ea250fdd55da901a36
Alejandro Marin <alejandromarin199901@gmail.com>
2024-02-02 10:31:19 -0300

    Creating the footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Heading } from "./_components/heading";
 import { Heroes } from "./_components/heroes";
 
@@ -8,7 +9,9 @@ const MarketingPage = () => {
         <Heading />
         <Heroes />
       </div>
+      <Footer />
     </div>
+    
   )
 }
 

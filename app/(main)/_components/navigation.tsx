bd39df1eddf8fd98e5cb4f97e18095240e0a1705
Alejandro Marin <alejandromarin199901@gmail.com>
2024-02-21 11:29:48 -0300

    User Settings

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 import { MenuIcon } from "lucide-react";
+import { UserItem } from "./user-item";
 
 
 
@@ -110,7 +111,7 @@ export const Navigation = () => {
                     <ChevronsLeft className="h-6 w-6" />
                 </div>
                 <div>
-                    <p>Action items</p>
+                    <UserItem />
                 </div>
                 <div className="mt-4">
                     <p>Documents</p>

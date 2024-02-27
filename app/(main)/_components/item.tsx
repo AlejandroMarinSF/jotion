835b40119314d203e7011eb82b397b46cbba3bad
Alejandro Marin <alejandromarin199901@gmail.com>
2024-02-27 10:14:48 -0300

    Sidebar item

---
+++ app/(main)/_components/item.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { LucideIcon } from "lucide-react";
+
+interface ItemProps {
+    label: string;
+    onClick: () => void;
+    icon: LucideIcon;
+}
+
+export const Item = ({
+    label,
+    onClick,
+    icon,
+}: ItemProps) => {
+    return (
+        <div>
+            {label}
+        </div>
+    )
+}
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { ChevronsLeft } from "lucide-react";
+import { ChevronsLeft, PlusCircle } from "lucide-react";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
+import { useQuery } from "convex/react"
 import { MenuIcon } from "lucide-react";
 import { UserItem } from "./user-item";
+import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
 
 
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
+    const documents = useQuery(api.documents.get);
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
     const navbarRef = useRef<ElementRef<"div">>(null);
@@ -112,9 +116,12 @@ export const Navigation = () => {
                 </div>
                 <div>
                     <UserItem />
+                    <Item onClick={() => {}} label="New page" icon={PlusCircle} />
                 </div>
                 <div className="mt-4">
-                    <p>Documents</p>
+                    {documents?.map((document) => (
+                        <p key={document._id}>{document.title}</p>
+                    ))}
                 </div>
                 <div onMouseDown={handleMouseDown} onClick={resetWidth} className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"/>
             </aside>
